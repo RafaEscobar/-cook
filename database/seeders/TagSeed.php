2f862aa77331deabcfbd02a92df7cfc630fd4dd7
rafa <rescobar@bit.lat>
2024-11-12 17:02:34 -0600

    Creación de seeders

---
+++ database/seeders/TagSeed.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TagSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
