9c74846866cdf408c260ca4bc66c2ac5a852b6d9
rafa <rescobar@bit.lat>
2024-11-14 12:12:46 -0600

    Agrega categorias para las recetas

--- database/seeders/RecipeCategorySeed.php
+++ database/seeders/RecipeCategorySeed.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\RecipeCategory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,62 @@ class RecipeCategorySeed extends Seeder
      */
     public function run(): void
     {
-        //
+        RecipeCategory::create(['category' => 'Desayunos']);
+        RecipeCategory::create(['category' => 'Comidas rápidas']);
+        RecipeCategory::create(['category' => 'Cenas ligeras']);
+        RecipeCategory::create(['category' => 'Postres']);
+        RecipeCategory::create(['category' => 'Comida vegana']);
+        RecipeCategory::create(['category' => 'Comida sin gluten']);
+        RecipeCategory::create(['category' => 'Salsas']);
+        RecipeCategory::create(['category' => 'Ensaladas']);
+        RecipeCategory::create(['category' => 'Comidas familiares']);
+        RecipeCategory::create(['category' => 'Recetas fáciles']);
+        RecipeCategory::create(['category' => 'Comida saludable']);
+        RecipeCategory::create(['category' => 'Bebidas']);
+        RecipeCategory::create(['category' => 'Aperitivos']);
+        RecipeCategory::create(['category' => 'Recetas rápidas']);
+        RecipeCategory::create(['category' => 'Comida mexicana']);
+        RecipeCategory::create(['category' => 'Comida italiana']);
+        RecipeCategory::create(['category' => 'Comida asiática']);
+        RecipeCategory::create(['category' => 'Comida vegetariana']);
+        RecipeCategory::create(['category' => 'Sopas']);
+        RecipeCategory::create(['category' => 'Panes']);
+        RecipeCategory::create(['category' => 'Comida para niños']);
+        RecipeCategory::create(['category' => 'Comida ligera']);
+        RecipeCategory::create(['category' => 'Recetas keto']);
+        RecipeCategory::create(['category' => 'Comida rápida']);
+        RecipeCategory::create(['category' => 'Comida casera']);
+        RecipeCategory::create(['category' => 'Recetas de temporada']);
+        RecipeCategory::create(['category' => 'Comida de fiesta']);
+        RecipeCategory::create(['category' => 'Comida típica']);
+        RecipeCategory::create(['category' => 'Comida para llevar']);
+        RecipeCategory::create(['category' => 'Desayunos saludables']);
+        RecipeCategory::create(['category' => 'Cenas fáciles']);
+        RecipeCategory::create(['category' => 'Recetas rápidas']);
+        RecipeCategory::create(['category' => 'Postres ligeros']);
+        RecipeCategory::create(['category' => 'Comida vegana']);
+        RecipeCategory::create(['category' => 'Comidas rápidas']);
+        RecipeCategory::create(['category' => 'Recetas vegetarianas']);
+        RecipeCategory::create(['category' => 'Sopas caseras']);
+        RecipeCategory::create(['category' => 'Recetas caseras']);
+        RecipeCategory::create(['category' => 'Comidas familiares']);
+        RecipeCategory::create(['category' => 'Ensaladas frescas']);
+        RecipeCategory::create(['category' => 'Comida mexicana']);
+        RecipeCategory::create(['category' => 'Comida asiática']);
+        RecipeCategory::create(['category' => 'Recetas keto']);
+        RecipeCategory::create(['category' => 'Comida italiana']);
+        RecipeCategory::create(['category' => 'Aperitivos fáciles']);
+        RecipeCategory::create(['category' => 'Comida ligera']);
+        RecipeCategory::create(['category' => 'Bebidas saludables']);
+        RecipeCategory::create(['category' => 'Comida sin gluten']);
+        RecipeCategory::create(['category' => 'Salsas caseras']);
+        RecipeCategory::create(['category' => 'Recetas para niños']);
+        RecipeCategory::create(['category' => 'Comida rápida']);
+        RecipeCategory::create(['category' => 'Comida de fiesta']);
+        RecipeCategory::create(['category' => 'Comidas saludables']);
+        RecipeCategory::create(['category' => 'Recetas de temporada']);
+        RecipeCategory::create(['category' => 'Recetas gourmet']);
+        RecipeCategory::create(['category' => 'Recetas tradicionales']);
+        RecipeCategory::create(['category' => 'Postres veganos']);
     }
 }
--- database/seeders/TagSeed.php
+++ database/seeders/TagSeed.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,33 @@ class TagSeed extends Seeder
      */
     public function run(): void
     {
-        //
+        Tag::create(['tag' => 'Desayuno']);
+        Tag::create(['tag' => 'Cena']);
+        Tag::create(['tag' => 'Postres']);
+        Tag::create(['tag' => 'Ensaladas']);
+        Tag::create(['tag' => 'Sopas']);
+        Tag::create(['tag' => 'Bebidas']);
+        Tag::create(['tag' => 'Aperitivo']);
+        Tag::create(['tag' => 'Galletas']);
+        Tag::create(['tag' => 'Pan']);
+        Tag::create(['tag' => 'Pasta']);
+        Tag::create(['tag' => 'Pizza']);
+        Tag::create(['tag' => 'Carnes']);
+        Tag::create(['tag' => 'Vegetales']);
+        Tag::create(['tag' => 'Vegana']);
+        Tag::create(['tag' => 'Keto']);
+        Tag::create(['tag' => 'SinGluten']);
+        Tag::create(['tag' => 'Fácil']);
+        Tag::create(['tag' => 'Casero']);
+        Tag::create(['tag' => 'Comida mexicana']);
+        Tag::create(['tag' => 'Comida italiana']);
+        Tag::create(['tag' => 'Comida asiática']);
+        Tag::create(['tag' => 'Comida tradicional']);
+        Tag::create(['tag' => 'Saludable']);
+        Tag::create(['tag' => 'Dulces']);
+        Tag::create(['tag' => 'Light']);
+        Tag::create(['tag' => 'Navidad']);
+        Tag::create(['tag' => 'Invierno']);
+        Tag::create(['tag' => 'Fiesta']);
     }
 }
