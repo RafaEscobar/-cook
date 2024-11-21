c49daf3111ef61633214a2e4495f1b0eeeb7f7bc
rafa <rescobar@bit.lat>
2024-11-21 17:01:52 -0600

    Instalación de sanctum

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+
+    public function getUser()
+    {
+
+    }
+}
