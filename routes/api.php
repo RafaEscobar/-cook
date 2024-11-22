89ddb33c2a5efe51bfba6ed2b46443c0e60543b8
rafa <rescobar@bit.lat>
2024-11-22 08:47:28 -0600

    Recolocando carpetas de request

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,15 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
-    ROute::get('/user', [AuthController::class, 'getUser']);
+    Route::get('/get-user', [AuthController::class, 'getUser']);
+    Route::apiResource('users', UserController::class);
 });
 
 
