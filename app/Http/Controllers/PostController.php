c2e7157f7761579572a0fa2dbf48c14fc5c1ae50
rafa <rescobar@bit.lat>
2024-11-22 10:46:39 -0600

    Hasta inicio de POSTS

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Error;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        try {
+            $posts = Post::all();
+            return view('posts.index', compact('posts'));
+        } catch (\Throwable $th) {
+            throw new Error($th->getMessage());
+        }
+    }
+
+    public function create()
+    {
+        try {
+            return view('posts.create');
+        } catch (\Throwable $th) {
+            throw new Error($th->getMessage());
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,15 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('users', UserController::class);
+    Route::apiResource('/users', UserController::class)->only(['update', 'show', 'destroy']);
+    Route::apiResource('/posts', PostController::class);
 });
 
 
