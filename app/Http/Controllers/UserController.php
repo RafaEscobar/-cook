bee8c731f3810f323bfa670e32ea59bd32122abc
rafa <rescobar@bit.lat>
2024-11-22 10:33:28 -0600

    Agrega show y delete a API de users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,4 +17,23 @@ class UserController extends Controller
             return response()->json(['message' => $th->getMessage()], 500);
         }
     }
+
+    public function show(User $user)
+    {
+        try {
+            return new UserResource($user);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th->getMessage()], 500);
+        }
+    }
+
+    public function destroy(User $user)
+    {
+        try {
+            $user->delete();
+            return response()->json(['message' => 'El usuario ha sido eliminado']);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th->getMessage()], 500);
+        }
+    }
 }
