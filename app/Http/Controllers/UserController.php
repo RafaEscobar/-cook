439b16e5996603afb19d266e04d885860e350e52
rafa <rescobar@bit.lat>
2024-12-06 13:30:39 -0600

    Optimizando código

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,36 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Http\Requests\Update\UserUpdateRequest;
 use App\Http\Resources\Resources\UserResource;
 use App\Models\User;
 
 class UserController extends Controller
 {
-    public function update(UserUpdateRequest $request, User $user)
+    private function findUser($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    public function update(UserUpdateRequest $request, $id)
     {
         try {
+            $user = $this->findUser($id);
             $user->update($request->all());
             return new UserResource($user);
+        } catch (ModelNotFoundException $e){
+            return response()->json(["message" => "Usuario no encontrado"], 404);
         } catch (\Throwable $th) {
             return response()->json(['message' => $th->getMessage()], 500);
         }
     }
 
-    public function show(User $user)
+    public function show($id)
     {
         try {
+            $user = $this->findUser($id);
             return new UserResource($user);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(["message" => "Usuario no encontrado"], 404);
         } catch (\Throwable $th) {
             return response()->json(['message' => $th->getMessage()], 500);
         }
     }
 
-    public function destroy(User $user)
+    public function destroy($id)
     {
         try {
+            $user = $this->findUser($id);
             $user->delete();
             return response()->json(['message' => 'El usuario ha sido eliminado'], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(["message" => "Usuario no encontrado"], 404);
         } catch (\Throwable $th) {
             return response()->json(['message' => $th->getMessage()], 500);
         }
